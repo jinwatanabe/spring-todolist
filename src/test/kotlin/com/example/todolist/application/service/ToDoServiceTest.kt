9322cadbea9be74bd2d2800620f27777509e40fd
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-01-13 15:05:10 +0900

    ToDoService test add

---
+++ src/test/kotlin/com/example/todolist/application/service/ToDoServiceTest.kt
@@ -0,0 +1,26 @@
+import com.example.todolist.application.service.ToDoService
+import com.example.todolist.infrastructure.repository.ToDoModel
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+
+@SpringBootTest(classes = [ToDoService::class])
+class ToDoServiceTest() {
+
+    @MockBean
+    private lateinit var toDoRepository: ToDoRepository
+
+    @Autowired
+    private lateinit var toDoService: ToDoService
+
+    @Test
+    fun `getToDos returns a list of todos`() {
+        Mockito.`when`(toDoRepository.findAll()).thenReturn(listOf(ToDoModel(1,"test", false)))
+        val toDoList = toDoService.getToDos()
+        assert(toDoList[0].title == "test")
+        assert(!toDoList[0].done)
+    }
+
+}
--- src/test/kotlin/com/example/todolist/presentation/controller/ToDoControllerTest.kt
+++ src/test/kotlin/com/example/todolist/presentation/controller/ToDoControllerTest.kt
@@ -2,15 +2,12 @@ import com.example.todolist.ToDoController
 import com.example.todolist.TodolistApplication
 import com.example.todolist.application.service.ToDoService
 import com.example.todolist.domain.model.ToDo
-import com.example.todolist.presentation.form.ToDoInfo
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
