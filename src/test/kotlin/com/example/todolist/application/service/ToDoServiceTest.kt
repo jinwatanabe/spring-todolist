7d11fe05f91c84f6b485e1226d3a038811595614
jinwatanabe <jin.watanabe.6g@gmail.com>
2023-01-14 21:29:39 +0900

    Add create service test

--- src/test/kotlin/com/example/todolist/application/service/ToDoServiceTest.kt
+++ src/test/kotlin/com/example/todolist/application/service/ToDoServiceTest.kt
@@ -1,7 +1,10 @@
 import com.example.todolist.application.service.ToDoService
+import com.example.todolist.domain.model.ToDo
 import com.example.todolist.infrastructure.repository.ToDoModel
+import com.example.todolist.presentation.form.ToDoInfo
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.mockito.Mockito.atLeastOnce
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
@@ -21,5 +24,14 @@ class ToDoServiceTest() {
         val toDoList = toDoService.getToDos()
         assert(toDoList[0].title == "test")
         assert(!toDoList[0].done)
+    }
+
+    @Test
+    fun `registerToDo registers a todo`() {
+        val toDo = ToDo(0, "test", false)
+        Mockito.`when`(toDoRepository.register(toDo)).then{}
+
+        toDoService.register(toDo)
+        Mockito.verify(toDoRepository, atLeastOnce()).register(toDo)
     }
 }
